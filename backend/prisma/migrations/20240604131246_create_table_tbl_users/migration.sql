7fd9578ee033c4c353ca4bdea0c1100666ae648a
Ahmad Huda Salam <ahmadhudasalam9@gmail.com>
2024-06-04 20:16:47 +0700

    backend : tes api login & register

---
+++ backend/prisma/migrations/20240604131246_create_table_tbl_users/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `tbl_users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER',
+    `creation_time` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_time` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `archived` BOOLEAN NOT NULL DEFAULT false,
+
+    UNIQUE INDEX `tbl_users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
