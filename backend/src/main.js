0c6c6098133f010c1716002ef29574c6ed5043b3
Kevin Marchelino <65926701+mavinpro@users.noreply.github.com>
2024-06-20 11:43:37 +0700

    Update main.js change port to 7184

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,6 +1,6 @@
 import { web } from "./application/web.js";
 import { logger } from "./application/logging.js";
 
-web.listen(5000, () => {
+web.listen(7184, () => {
   logger.info("Server is running on port 5000");
 });
